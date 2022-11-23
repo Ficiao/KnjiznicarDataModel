6c4499dab5a1464c043b7d9b4deb637e2e405e1e
Ficiao <filipmtvn@gmail.com>
2022-11-23 12:19:40 +0100

    added username to playerconnected message

--- Message/PlayerConnectedMessage.cs
+++ Message/PlayerConnectedMessage.cs
@@ -5,6 +5,7 @@ namespace KnjiznicarDataModel.Message
     class PlayerConnectedMessage :BaseMessage
     {
         public PlayerData playerData;
+        public string username;
         public string playerIp;
 
         public PlayerConnectedMessage() : base(MessageType.PlayerConnected)
