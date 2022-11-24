ea9292e336549bee125cfa6788156beb4f48357d
Ficiao <filipmtvn@gmail.com>
2022-11-24 20:44:49 +0100

    player id

--- Model/PlayerData.cs
+++ Model/PlayerData.cs
@@ -5,6 +5,7 @@ namespace KnjiznicarDataModel
     public class PlayerData
     {
         public string playerName;
+        public string playerId;
         public int level;
         public List<Item> items;
         public int adventureLevel;
