fac9f893fc3ae441b9b2e247c3b6bfae8cb2a785
Ficiao <filipmtvn@gmail.com>
2022-12-02 15:04:35 +0100

    Changed player coordinates message that server sends

--- Message/PlayerCoordinatesMessage.cs
+++ Message/PlayerCoordinatesMessage.cs
@@ -1,15 +1,22 @@
 ﻿using KnjiznicarDataModel.Enum;
+using System.Collections.Generic;
 
 namespace KnjiznicarDataModel.Message
 {
     class PlayerCoordinatesMessage : BaseMessage
     {
-        public string playerId;
-        public float[] position = new float[3];
-        public float[] rotation = new float[3];
+        public class PlayerCoordinates
+        {
+            public string playerId;
+            public float[] position = new float[3];
+            public float[] rotation = new float[3];
+        }
+
+        public List<PlayerCoordinates> PlayerPositions;
 
         public PlayerCoordinatesMessage() : base(MessageType.PlayerCoordinates)
         {
+            PlayerPositions = new List<PlayerCoordinates>();
         }
     }
 }
