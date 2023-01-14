00e349d2ccb7bc32b7b8cc5752b26ac04745d59b
Ficiao <filipmtvn@gmail.com>
2023-01-14 14:29:28 +0100

    Bandwith optimization

--- Message/BaseMessage/BaseMessage.cs
+++ Message/BaseMessage/BaseMessage.cs
@@ -1,9 +1,11 @@
 ﻿using KnjiznicarDataModel.Enum;
+using Newtonsoft.Json;
 
 namespace KnjiznicarDataModel.Message
 {
     public class BaseMessage
     {
+        [JsonProperty("MT")]
         public MessageType MessageType { get; }
 
         protected BaseMessage(MessageType type)
